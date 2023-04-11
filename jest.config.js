5a14fe58d69ec5983d4981863ec2862b1344c9f8
laszlo-horvath <horvathlaszlo88@gmail.com>
2023-04-11 16:58:29 +0200

    Configure Jest for unit testing with Typescript.

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)?$': ['ts-jest', { isolatedModules: true }],
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^redux/(.*)$": "<rootDir>/src/redux/$1",
+    "config": "<rootDir>/src/config/index.ts",
+    "^lib/(.*)$": "<rootDir>/src/lib/$1",
+  },
+}
